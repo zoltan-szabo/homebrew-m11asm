ab6396a9de86b9038ce106623fe7bd7fd47195ff
zoltan-szabo <zoltan@szabo.nz>
2026-07-10 17:21:32 +1200

    m11asm 0.4.1

--- Formula/m11asm.rb
+++ Formula/m11asm.rb
@@ -1,9 +1,9 @@
 class M11asm < Formula
   desc "MACRO-11 assembler for DCJ-11 / PDP-11"
   homepage "https://github.com/zoltan-szabo/m11asm"
-  url "https://github.com/zoltan-szabo/m11asm/releases/download/v0.4.0/m11asm-v0.4.0-macos-universal.zip"
-  sha256 "86a364eaa21c78761d2f6ff159e41e6cd6dae3cecf010f0d000455611ce1eb14"
-  version "0.4.0"
+  url "https://github.com/zoltan-szabo/m11asm/releases/download/v0.4.1/m11asm-v0.4.1-macos-universal.zip"
+  sha256 "5d823b71b1fd562d86cf226fd095f234dbeb9c12c68ea5bfa891cf73c5dc685a"
+  version "0.4.1"
   license "MIT"
 
   depends_on :macos
